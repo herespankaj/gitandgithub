aac3b4a3a2a4a9ff08b0ac9a675c4be6429493a4
Pankaj <herespankaj.com>
2020-12-02 18:33:14 +0000

    Added gitandgithub project to Local Repository

---
+++ src/main/java/com/pankaj/test/gitandgithub/App.java
@@ -0,0 +1,19 @@
+package com.pankaj.test.gitandgithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App { 
+	
+	public void addData(String data) { 
+		
+		System.out.println("The data is " + data);
+	}
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
