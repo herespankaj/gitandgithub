c38c7bb6f932110ad584576147b61f59a23f5e55
Pankaj <herespankaj.com>
2020-12-02 21:13:26 +0000

    Added Regular class

---
+++ src/main/java/com/pankaj/test/gitandgithub/Regular.java
@@ -0,0 +1,15 @@
+package com.pankaj.test.gitandgithub;
+
+public class Regular {
+
+	public Regular() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		new Regular().getClass();
+		
+	}
+		
+}
