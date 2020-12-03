0a0cd9b7424d78a347ea8b1b904845c7949e2adb
herespankaj <herespankaj@gmail.com>
2020-12-03 13:57:30 +0000

    Update Regular.java
    New Method Added to Check

--- src/main/java/com/pankaj/test/gitandgithub/Regular.java
+++ src/main/java/com/pankaj/test/gitandgithub/Regular.java
@@ -5,7 +5,12 @@ public class Regular {
 	public Regular() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public void NewMethodAddedToCheck(){ 
+		
+		System.out.println("This is a New Method Added to the Class !!!"); 
+	} 
+	
 	public static void main(String[] args) {
 		
 		new Regular().getClass();
